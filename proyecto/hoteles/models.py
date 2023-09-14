29ff8c61a8136845c0200d627bfe9b40d6ecb1f7
danianre <107223922+danianre@users.noreply.github.com>
2023-09-13 22:44:10 -0500

    Update models.py

--- proyecto/hoteles/models.py
+++ proyecto/hoteles/models.py
@@ -4,21 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-#Formato tabla
+#Formato tabla ejemplo
 class Tabla(models.Model):
-    user = models.ForeingKey(UserTabla, on_delete = models.CASCADE)
     nombre = models.CharField(max_length=50)
     porcentaje = models.DecimalField(max_digits=5, decimal_places=2)
     fecha = models.DateField(auto_now_add=True)
-    producto = models.ManyToManyField(ProductoTabla) #muchos a muchos
     cantidad = models.IntegerField()
-
-
-    class Meta:
-        ordering = ['nombre']
-
-    def get_productos(self):
-        return "\n".join([p.nombre for p in self.producto.all()])
 
     def __str__(self):
         return self.nombre
