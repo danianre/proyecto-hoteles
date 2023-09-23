a6bc0d917ca0260ab1d501cbd00425e368a6ee04
danianre <danianre28@gmail.com>
2023-09-23 16:36:54 -0500

    Se removió objeto innecesario

--- proyecto/hoteles/modelo/caracteristicaPorInmueble.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from caracteristica import Caracteristica
-from inmueble import Inmueble
-
-class CaracteristicasPorInmueble(models.Model):
-    idCaracteristica = models.ForeignKey(Caracteristica, on_delete=models.CASCADE)
-    idInmueble = models.ForeignKey(Inmueble, on_delete=models.CASCADE)
