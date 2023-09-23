f3334aae574c5a7fd2212aa1193bb0dc8d4f9c3e
danianre <danianre28@gmail.com>
2023-09-23 16:38:03 -0500

    Se agregaron los métodos

--- proyecto/hoteles/modelo/inmueble.py
+++ proyecto/hoteles/modelo/inmueble.py
@@ -1,7 +1,8 @@
 from django.db import models
-from ciudad import Ciudad
-from sector import Sector
-from direccion import Direccion
+from proyecto.hoteles.modelo.caracteristica import Caracteristica
+from proyecto.hoteles.modelo.ciudad import Ciudad
+from proyecto.hoteles.modelo.sector import Sector
+from proyecto.hoteles.modelo.direccion import Direccion
 
 
 class Inmueble(models.Model):
@@ -23,7 +24,39 @@ class Inmueble(models.Model):
     idSector = models.ForeignKey(Sector, on_delete=models.CASCADE)
     idDireccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
     idCiudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    caracteristicas = models.ManyToManyField(Caracteristica)
 
     def __str__(self):
         return self.nombre
     
+
+class InmuebleDAO(models.Model):
+    @classmethod
+    def ver_inmuebles(cls):
+        # Recuperar todos los inmuebles desde la base de datos
+        inmuebles = Inmueble.objects.all()
+        return inmuebles
+
+    @classmethod
+    def filtrar_por_ubicacion_y_tipo(cls, ubicacion, tipo_inmueble):
+        # Filtra los inmuebles por ubicación y tipo de inmueble
+        inmuebles = cls.objects.filter(ubicacion=ubicacion, tipo_inmueble=tipo_inmueble)
+        return inmuebles
+    
+    @classmethod
+    def filtrar_por_rango_de_precio(cls, precio_min, precio_max):
+        # Filtra los inmuebles por un rango de precios (precio_min <= precio <= precio_max)
+        inmuebles = cls.objects.filter(precio__gte=precio_min, precio__lte=precio_max)
+        return inmuebles
+    
+    @classmethod
+    def filtrar_por_antiguedad(cls, antiguedad):
+        # Filtra los inmuebles por antiguedad
+        inmuebles = cls.objects.filter(antiguedad=antiguedad)
+        return inmuebles
+    
+    @classmethod
+    def filtrar_por_caracteristicas(cls, caracteristicas):
+        # Filtra los inmuebles que tienen todas las características especificadas
+        inmuebles = cls.objects.filter(caracteristicas__in=caracteristicas).distinct()
+        return inmuebles
--- proyecto/hoteles/modelo/usuario.py
+++ proyecto/hoteles/modelo/usuario.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
+from proyecto.hoteles.modelo import DataSource
 
 
 class Usuario(models.Model):
@@ -15,3 +16,85 @@ class Usuario(models.Model):
         clave = 'mMUj0DrIK6vgtdIYepkIxN'
         self.password = make_password(self.password, clave)
         super().save(**kwargs)
+
+class UsuarioDAO(models.Model):
+    @classmethod
+    def autenticar_usuario(cls, email, password):
+        # Se crea la instancia de DataSource para hacer la conexión
+        data_source = DataSource()
+
+        # Se llama al método ejecutarConsulta para devolver el usuario
+        # que cumple con el email y contraseña recibidos del login
+        data_table = data_source.ejecutar_consulta(
+            "SELECT * FROM usuario WHERE email = %s AND password = %s",
+            (email, password)
+        )
+
+        usuario = None
+
+        # Si data_table contiene una fila, significa que se encontró el usuario en la base de datos
+        if len(data_table) == 1:
+            usuario_data = data_table[0]
+            usuario = cls(
+                idUsuario=usuario_data["idUsuario"],
+                email=usuario_data["email"],
+                password=usuario_data["password"],
+                nombre=usuario_data["nombre"],
+                apellido=usuario_data["apellido"],
+                edad=usuario_data["edad"],
+            )
+
+        return usuario
+    
+    @classmethod
+    def registrar_usuario(cls, usuario):
+        data_source = DataSource()
+
+        stmt = """
+            INSERT INTO usuario (nombre, apellido, email, password, edad)
+            VALUES (%s, %s, %s, %s, %s)
+        """
+
+        resultado = data_source.ejecutar_actualizacion(
+            stmt,
+            (
+                usuario.email,
+                usuario.password,
+                usuario.nombre,
+                usuario.apellido,  
+                usuario.edad,
+            ),
+        )
+
+        return resultado
+    
+    @classmethod
+    def ver_usuarios(cls):
+        data_source = DataSource()
+
+        data_table = data_source.ejecutar_consulta("SELECT * FROM usuario")
+
+        usuarios = []
+
+        for usuario_data in data_table:
+            usuario = cls(
+                idUsuario=usuario_data["idUsuario"],
+                email=usuario_data["email"],
+                password=usuario_data["password"],
+                nombre=usuario_data["nombre"],
+                apellido=usuario_data["apellido"],
+                idGenero=usuario_data["idGenero"],
+            )
+            usuarios.append(usuario)
+
+        return usuarios
+    
+    @classmethod
+    def eliminar_usuario(cls, idUsuario):
+        data_source = DataSource()
+
+        stmt = "DELETE FROM usuario WHERE idUsuario = %s"
+
+        resultado = data_source.ejecutar_actualizacion(stmt, (idUsuario,))
+
+        return resultado
