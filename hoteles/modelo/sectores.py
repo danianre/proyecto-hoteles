417506d078609f3167e83e579125657b79247c0e
danianre <danianre28@gmail.com>
2023-10-01 16:17:45 -0500

    .

--- hoteles/modelo/sectores.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Sectores(models.Model):
-    idSector = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.nombre
--- hoteles/modelo/tiposDeCaracteristicas.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class TiposDeCaracteristicas(models.Model):
-    idtipoDeCaracteristicas = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=255)
