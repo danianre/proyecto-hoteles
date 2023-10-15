66d3e80dd9bfc7d46cac2a9453b2110eda0f7c04
danianre <danianre28@gmail.com>
2023-10-15 17:12:26 -0500

    Modificacion

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,6 +1,10 @@
 import ProductDesc from '../components/ProductDesc/ProductDesc'
 import Header from '../components/Header/Header'
 import Hero from '../components/Hero/Hero'
+import WhoSection from '../components/WhoSection/WhoSection'
+import MayFindSect from '../components/MayFind/MayFindSect'
+import Footer from '../components/Footer/Footer'
+import JoinUs from '../components/JoinUs/JoinUs'
 
 
 function Landing(){
@@ -8,7 +12,11 @@ function Landing(){
         <>
             <Header/>
             <Hero/>
+            <WhoSection/>
             <ProductDesc/>
+            <MayFindSect/>
+            <JoinUs/>
+            <Footer/>
         </>
     )
 }
