17625ad35ac4312755dbd9171e613b24b697a414
danianre <danianre28@gmail.com>
2023-10-22 03:14:58 -0500

    Se agregaron validaciones

--- hoteles/controlador/UsuariosPOST.py
+++ hoteles/controlador/UsuariosPOST.py
@@ -2,26 +2,32 @@ from django.http import JsonResponse
 from hoteles.modelo.usuarios import Usuarios
 from django.contrib.auth import get_user_model
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
+@csrf_exempt
+def autenticar_usuario(request):
+    if request.method == 'POST':
+        try:
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+
+            
+            # Buscar un usuario por correo y contraseña en la base de datos
+            Usuario = get_user_model()
+            Usuario.objects.get(email=email, password=password)
+
+            # Aquí se puede realizar cualquier acción adicional si el usuario se autentica exitosamente
+            return JsonResponse({'message': 'Usuario autenticado exitosamente'})
+        except Usuario.DoesNotExist:
+            # Manejar el caso en el que el usuario no se encuentre en la base de datos o la autenticación sea incorrecta
+            return JsonResponse({'error': 'Credenciales inválidas'}, status=401)
+    else:
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
 
 
-# @require_POST
-# def autenticar_usuario(request):
-#     if request.method == 'POST':
-#         try:
-#             email = request.POST.get('email')
-#             password = request.POST.get('password')
-
-#             # Buscar un usuario por correo y contraseña en la base de datos
-#             Usuario = get_user_model()
-#             Usuario.objects.get(email=email, password=password)
-#             # Aquí se puede realizar cualquier acción adicional si el usuario se autentica exitosamente
-#             return JsonResponse({'message': 'Usuario autenticado exitosamente'})
-#         except Usuario.DoesNotExist:
-#             # Manejar el caso en el que el usuario no se encuentre en la base de datos o la autenticación sea incorrecta
-#             return JsonResponse({'error': 'Credenciales inválidas'}, status=401)
-#     else:
-#         return JsonResponse({'error': 'Método no permitido'}, status=405)
-
+@csrf_exempt
 def registrar_usuario(request):
     if request.method == 'POST':
         try:
@@ -34,41 +40,48 @@ def registrar_usuario(request):
                 'edad': request.POST.get('edad'),
             }
 
-            # Crear un nuevo objeto Usuario y guardarlo en la base de datos
-            usuario = Usuarios(
-                email=usuario_data['email'],
-                password=usuario_data['password'],
-                nombre=usuario_data['nombre'],
-                apellido=usuario_data['apellido'],
-                edad=usuario_data['edad'],
-            )
-            usuario.save()
+            # Imprimir los datos para verificar que se reciben correctamente
+            print("Datos recibidos:", usuario_data)
+
+            # Validación de campos obligatorios
+            required_fields = ['email', 'password', 'nombre', 'apellido', 'edad']
+            if all(field in usuario_data for field in required_fields):
+                # Todos los campos obligatorios están presentes, proceder con la creación y guardado del usuario
+                usuario = Usuarios(
+                    email=usuario_data['email'],
+                    password=usuario_data['password'],
+                    nombre=usuario_data['nombre'],
+                    apellido=usuario_data['apellido'],
+                    edad=usuario_data['edad'],
+                )
+                usuario.save()
 
             # Devolver una respuesta de éxito en formato JSON
             return JsonResponse({'message': 'Usuario registrado exitosamente'})
 
         except Exception as e:
-            # Manejar cualquier excepción que pueda ocurrir al guardar el usuario
-            # Por ejemplo, manejar la excepción de violación de restricción única en el campo 'email'
-            return JsonResponse({'error': 'Error al registrar el usuario'}, status=400)
+            # Capturar la excepción y devolver un mensaje de error detallado
+            return JsonResponse({'error': f'Error al registrar el usuario: {str(e)}'}, status=400)
 
     else:
         # Manejar el caso en el que no se realice una solicitud POST
         # Puedes devolver un error o realizar otra acción aquí
         return JsonResponse({'error': 'Método no permitido'}, status=405)
-
-# def ver_usuariosGET(request):
-#     if request.method == 'GET':
-#         # Recuperar todos los usuarios desde la base de datos utilizando el ORM de Django
-#         usuarios = Usuarios.objects.all()
-#         usuarios_data = [{'id': usuario.idUsuario, 'nombre': usuario.nombre, 'apellido': usuario.apellido, 'email': usuario.email, 'edad': usuario.edad} for usuario in usuarios]
-#         return JsonResponse({'usuarios': usuarios_data})
-#     else:
-#         # Manejar el caso en el que no se realice una solicitud GET
-#         # Puedes devolver un error o realizar otra acción aquí
-#         return JsonResponse({'error': 'Método no permitido'}, status=405)
+    
     
 
+def ver_usuariosGET(request):
+    if request.method == 'GET':
+        # Recuperar todos los usuarios desde la base de datos utilizando el ORM de Django
+        usuarios = Usuarios.objects.all()
+        usuarios_data = [{'id': usuario.idUsuario, 'nombre': usuario.nombre, 'apellido': usuario.apellido, 'email': usuario.email, 'edad': usuario.edad} for usuario in usuarios]
+        return JsonResponse({'usuarios': usuarios_data})
+    else:
+        # Manejar el caso en el que no se realice una solicitud GET
+        # Puedes devolver un error o realizar otra acción aquí
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
+    
+@csrf_exempt
 def ver_usuariosPOST(request):
     if request.method == 'POST':
         # Recuperaremos todos los usuarios y los devolveremos en formato JSON
--- hoteles/controlador/login.py
+++ hoteles/controlador/login.py
@@ -1,28 +1,37 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from hoteles.modelo import usuarios
+from hoteles.modelo.usuarios import UsuarioManager
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def login_view(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        
-        # Se utiliza método para autenticar al usuario
-        user = usuarios.UsuarioManager.autenticar_usuario(email, password)
-        
-        if user is not None:
-            # Si el usuario se autentica correctamente, inicia sesión y redirige a la página principal
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('principal') 
+        # Validación de campos requeridos
+        if 'email' in request.POST and 'password' in request.POST:
+            email = request.POST['email']
+            password = request.POST['password']
+            print(f"Email: {email}, Password: {password}")  # Agregar esta línea para imprimir los datos
         else:
-            # Si la autenticación falla, muestra un mensaje de error o vuelve a mostrar la vista de inicio de sesión
-            return render(request, 'login', {'error_message': 'Email o contraseña incorrectos'})
-    else:
-        return render(request, 'login')  # Muestra la vista de inicio de sesión
+            return JsonResponse({'error': 'Campos de email y contraseña requeridos'}, status=400)
 
-def principal_view(request):
-    # Vista para la página principal de usuarios
-    return render(request, 'principal')
+        # Se utiliza método para autenticar al usuario
+        user = UsuarioManager.autenticar_usuario(email, password)
+
+        if user is not None:
+            # Autenticación exitosa
+            user_auth = authenticate(request, email=email, password=password)
+            if user_auth is not None:
+                login(request, user)
+                return JsonResponse({'message': 'Usuario autenticado con éxito'})
+            else:
+                return JsonResponse({'error': 'Error en la autenticación de Django'}, status=400)
+        else:
+            return JsonResponse({'error': 'Error en la autenticación de la aplicación'}, status=400)
+    else:
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+# def principal_view(request):
+#     # Vista para la página principal de usuarios
+#     return render(request, 'principal')
 
